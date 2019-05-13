29ac25ebaabf3d9b9a7fdaf7d29bea46c2665586
filipesiegrist <filipebrasil8@gmail.com>
2019-05-12 23:41:21 -0300

    Adding a basic test to simulate the operation of the robot.

---
+++ matlab_codes/main_test.m
@@ -0,0 +1,20 @@
+clear;
+warning('off','all');
+
+% Main para teste do código
+
+%Parametros do robô:
+a1 = 15; % mm
+a2 = 45; % mm
+a3 = 83; % mm
+
+h = 85; % mm
+
+objetivo_x = 60; % mm
+objetivo_y = 60; % mm
+objetivo_z = 60; % mm
+
+[t1,t2,t3] = Inv_ARR5(h,a1,a2,a3,objetivo_x,objetivo_y,objetivo_z)
+
+[nx,ny,nz] = Dir_ARR5(h,a1,a2,a3,t1,t2,t3)
+
