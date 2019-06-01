57b0d98ab0b8d3a4c57ecb1fb919b90666b854c8
filipesiegrist <filipebrasil8@gmail.com>
2019-06-01 20:44:54 -0300

    Starting to fix the direct kinematics function.

--- src/ARR5_Kinematics/ARR5_Direct_Kinematics.cpp
+++ src/ARR5_Kinematics/ARR5_Direct_Kinematics.cpp
@@ -2,7 +2,7 @@
 * @Author: Filipe Ernesto Siegrist Gonçalves
 * @Date:   2019-05-26 15:37:39
 * @Last Modified by:   filipe-ufsc
-* @Last Modified time: 2019-05-28 18:37:17
+* @Last Modified time: 2019-05-30 15:03:14
 */
 
 #ifndef ARR5_DIRECT_KINEMATICS_CPP__
@@ -11,12 +11,13 @@
 #include "../ARR5_Data.h"
 #include "ARR5_Direct_Kinematics.h"
 
-position_t direct_kinematics(angle_t a1, angle_t a2,angle_t a3, distance_t d1) {
+position_t direct_kinematics(distance_t a1, distance_t a2,distance_t a3, distance_t d1, angle_t theta_1, angle_t theta_2, angle_t theta_3) {
     position_t posicao;
-    aux = a1 + a2*cos(a2) + a3*cos(a2+a3);
-    posicao.x = cos(a1)*aux;
-    posicao.y = sin(a1)*aux;
-    posicao.z =  -a2*sin(a2) - a3*⋅sin(a2+a3) + d1;
+    distance_t aux;
+    aux = a1 + a2*cos(theta_2) + a3*cos(theta_2+theta_3);
+    posicao.x = cos(theta_1)*aux;
+    posicao.y = sin(theta_1)*aux;
+    posicao.z =  -1*a2*sin(theta_2) - a3*sin(theta_2+theta_3) + d1;
     return posicao;    
 }
 
