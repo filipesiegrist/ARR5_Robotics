0d45f7c2f149e390424b467202b906872633fa4e
filipesiegrist <filipebrasil8@gmail.com>
2019-06-01 20:50:50 -0300

    Implementating the angle conversions.

--- src/ARR5_Angle_Conversions/ARR5_Angle_Conversions.cpp
+++ src/ARR5_Angle_Conversions/ARR5_Angle_Conversions.cpp
@@ -2,9 +2,25 @@
 * @Author: Filipe Ernesto Siegrist Gonçalves
 * @Date:   2019-05-26 16:48:02
 * @Last Modified by:   filipe-ufsc
-* @Last Modified time: 2019-05-26 16:48:16
+* @Last Modified time: 2019-06-01 20:50:33
 */
 #ifndef ARR5_ANGLE_CONVERSIONS_CPP__
 #define ARR5_ANGLE_CONVERSIONS_CPP__
+
+#include "../ARR5_Data.h"
+
+int fromJointToServoBase(angle_t baseAngle) {
+	return (int) (baseAngle - 89);
+}
+
+int fromJointToServoShoulder(angle_t shoulderAngle) {
+	return (int) (1.25*shoulderAngle + 40);
+}
+
+int fromJointToServoElbow(angle_t elbowAngle) {
+	#define B 1.11111111
+	#define A 105
+	return (int) (B*elbowAngle + A);
+}
 
 #endif /* end of include guard: ARR5_ANGLE_CONVERSIONS_CPP__ */
