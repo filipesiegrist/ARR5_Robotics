62618d992da6015eb1a76bacf54b12829fd3a98e
filipesiegrist <filipebrasil8@gmail.com>
2019-06-01 20:43:32 -0300

    Creating the prototype of the Angle conversion functions.

--- src/ARR5_Angle_Conversions/ARR5_Angle_Conversions.h
+++ src/ARR5_Angle_Conversions/ARR5_Angle_Conversions.h
@@ -1,4 +1,12 @@
 #ifndef ARR5_ANGLE_CONVERSIONS_H__
 #define ARR5_ANGLE_CONVERSIONS_H__
 
+#include "../ARR5_Data.h"
+
+int fromJointToServoBase(angle_t baseAngle);
+
+int fromJointToServoShoulder(angle_t shoulderAngle);
+
+int fromJointToServoElbow(angle_t elbowAngle);
+
 #endif /* end of include guard: ARR5_ANGLE_CONVERSIONS_H__ */
