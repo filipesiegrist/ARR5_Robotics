0e0c2baf317c9750f2f9e2195a5822c24d8abbe3
filipesiegrist <filipebrasil8@gmail.com>
2019-06-08 16:22:51 -0300

    Improving inverse kinematics comments

--- src/ARR5_Kinematics/ARR5_Inverse_Kinematics.h
+++ src/ARR5_Kinematics/ARR5_Inverse_Kinematics.h
@@ -19,6 +19,8 @@ Algumas instrucoes pra vc implementar:
 
 Tem que converter os angulos vindos das funções acos e atan2 de radianos pra graus. Ignora a conversao que usei no fim da funcao do matlab.
 
+robot_angles_t é uma estrutura que guarda três angulos, feita pra facilitar a sua vida. ela ta declarada no ARR5_Data.h
+
 Tem um arquivo no ARR5_Angle_Conversions que tem essas funções prontas, qualquer dúvida dá uma olhada no ARR5_Direct_Kinematics.
 
 O parametro elbow_up_or_down só vai dizer se o cotovelo do robo estara pra cima e pra baixo, pq temos duas respostas pra raiz quadrada. Vc vai colocar ele multiplicando na hora de calcular o s3 e multiplicando o p (psi) pra calcular o "th_2".
