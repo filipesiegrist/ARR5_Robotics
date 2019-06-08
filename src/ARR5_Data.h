1c95b5e736a818afe9797c626c83124a5bd46a24
filipesiegrist <filipebrasil8@gmail.com>
2019-06-08 15:44:53 -0300

    Adding a struct to have the robot joint angles and an enum to handle the two different orientations from inverse kinematics

--- src/ARR5_Data.h
+++ src/ARR5_Data.h
@@ -10,13 +10,26 @@
 
 typedef float angle_t;
 typedef float distance_t;
+
 typedef  enum{JOINT_1,JOINT_2,JOINT_3} joints_t;
+
+// Se quero o cotovelo pra cima, uso 1, senao uso -1.
+typedef enum basic_orientation{
+	ELBOW_UP = 1,
+	ELBOW_DOWN = -1
+} basic_orientation_t;
 
 typedef struct position {
     distance_t x;
     distance_t y;
     distance_t z;
 } position_t;
+
+typedef struct robot_angles {
+	angle_t theta_1;
+	angle_t theta_2;
+	angle_t theta_3;
+} robot_angles_t;
 
 
 #endif /* end of include guard: ARR5_DATA_H__ */
