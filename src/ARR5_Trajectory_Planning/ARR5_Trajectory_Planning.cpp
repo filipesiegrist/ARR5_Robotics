004cffa3ab62967910e3853149e9d9fe65257ce8
filipesiegrist <filipebrasil8@gmail.com>
2019-06-08 17:04:15 -0300

    Adding the basic trajectory functions and headers.

--- src/ARR5_Trajectory_Planning/ARR5_Trajectory_Planning.cpp
+++ src/ARR5_Trajectory_Planning/ARR5_Trajectory_Planning.cpp
@@ -1,6 +1,20 @@
 #ifndef ARR5_TRAJECTORY_PLANNING_CPP
 #define ARR5_TRAJECTORY_PLANNING_CPP 
 
-
+position_t move_x(position_t pos, distance_t delta){
+	position_t out = pos;
+	out.x += delta;
+	return out;
+}
+position_t move_y(position_t pos, distance_t delta){
+	position_t out = pos;
+	out.y += delta;
+	return out;
+}
+position_t move_z(position_t pos, distance_t delta){
+	position_t out = pos;
+	out.z += delta;
+	return out;
+}
 
 #endif
--- src/ARR5_Trajectory_Planning/ARR5_Trajectory_Planning.h
+++ src/ARR5_Trajectory_Planning/ARR5_Trajectory_Planning.h
@@ -1,6 +1,24 @@
 #ifndef ARR5_TRAJECTORY_PLANNING_H
 #define ARR5_TRAJECTORY_PLANNING_H 
 
+#include "../ARR5_Data.h"
 
+/*
+	Numero de pontos intermediarios gerados.
+*/
+#define N_PONTOS 5
 
-#endif
+/*
+
+Gera N_PONTOS pontos intermediarios na reta que liga p1 a p2.
+E joga-os no vetor estaticamente alocado (com N_PONTOS) out_pontos
+Esse vetor é do tipo position_t. Da uma olhada no ARR5_Data.h pra ver. Isso foi feito pra facilitar.
+
+*/
+void pontos_intermediarios(position_t p1, position_t p2, position_t out_pontos[]);
+
+// Geram posicoes deslocadas em x, y ou z de uma distancia delta.
+
+position_t move_x(position_t pos, distance_t delta);
+position_t move_y(position_t pos, distance_t delta);
+position_t move_z(position_t pos, distance_t delta);
