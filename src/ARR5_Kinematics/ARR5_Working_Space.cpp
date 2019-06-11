518e76df85711a95301cd4aec5563c70febc374a
filipesiegrist <filipebrasil8@gmail.com>
2019-06-11 20:30:41 -0300

    Adding the prototype of the function that verifies the reachalibity of the coordinate.

---
+++ src/ARR5_Kinematics/ARR5_Working_Space.cpp
@@ -0,0 +1,11 @@
+#ifndef __ARR5_WORKING_SPACE_CPP__
+#define __ARR5_WORKING_SPACE_CPP__
+
+#include "../ARR5_Data.h"
+
+
+bool is_point_on_working_space(distance_t d1, distance_t a1, distance_t a2, distance_t a3, position_t point, basic_orientation_t elbow_up_or_down) {
+	return false;
+}
+
+#endif
