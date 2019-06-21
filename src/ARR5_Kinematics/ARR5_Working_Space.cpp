0c18696021aea4dbc112632f83b6a8621b2afdec
Paulo Victor <paulovictor237@gmail.com>
2019-06-20 23:08:37 -0300

    Tudo Revisado
    revisado a cinematica inversa

--- src/ARR5_Kinematics/ARR5_Working_Space.cpp
+++ src/ARR5_Kinematics/ARR5_Working_Space.cpp
@@ -11,34 +11,42 @@ using namespace std;
 // Funcao que calcula o quadrado.
 distance_t SQ(distance_t X);
 
-bool is_point_on_working_space(distance_t d1, distance_t a1, distance_t a2, distance_t a3, position_t point, basic_orientation_t elbow_up_or_down) {
+bool is_point_on_working_space(distance_t d1, distance_t a1, distance_t a2, distance_t a3, position_t point, basic_orientation_t elbow_up_or_down=ELBOW_DOWN) {
+	// ELBOW_UP = -1,
+	// ELBOW_DOWN = 1
+    // elbow_up_or_down=ELBOW_DOWN;
 
     distance_t pwx = point.x;
 	distance_t pwy = point.y;
 	distance_t pwz = point.z;
 
 	robot_angles_t angles_out;
+    angles_out.theta_1 = 0;
+	angles_out.theta_2 = 0;
+	angles_out.theta_3 = 0;
 
-	distance_t pwh = sqrt(SQ(pwx)+SQ(pwy)) - a1;
+	distance_t pwh = sqrt(abs(SQ(pwx)+SQ(pwy))) - a1;
     distance_t pwz1=pwz;
     pwz = pwz - d1;
     
-
     float c3 = (SQ(pwh) + SQ(pwz) - SQ(a2) - SQ(a3)) / (2*a2*a3);
-	if(c3>1 || c3<-1)return false;
     //Verificar se o cosseno esta entre menos um e um!
+    if(c3 > 1 || c3 < -1)return false;
 
     float s3 = sqrt(abs(1-SQ(c3)))*elbow_up_or_down; // pode ser negativo
     float rad_th_3 = atan2(s3,c3);
 
 
-    float cphi = (SQ(pwh) + SQ(pwz) + SQ(a2) - SQ(a3)) / (2*a2*sqrt(SQ(pwh)+SQ(pwz)));
-	if(cphi>1 || cphi<-1)return false;
+    float cphi = (SQ(pwh) + SQ(pwz) + SQ(a2) - SQ(a3)) / (2*a2*sqrt(abs(SQ(pwh)+SQ(pwz))));
     //Verificar se o cosseno esta entre menos um e um!
+    if(cphi > 1 || cphi < -1)return false;
 
+    if(cphi>1)cphi=1;
+    if(cphi<-1)cphi=-1;
     float phi = acos(cphi);
     float beta = atan2(pwz,pwh);
     float rad_th_2 = beta - phi*elbow_up_or_down;
+
     float rad_th_1 = atan2(pwy,pwx);
     
     angle_t th_1= (angle_t) radiansToDegrees(rad_th_1);
@@ -53,21 +61,21 @@ bool is_point_on_working_space(distance_t d1, distance_t a1, distance_t a2, dist
     if(th_2 > MAX_ANGLE_2 || th_2 < MIN_ANGLE_2)return false;
     if(th_3 > MAX_ANGLE_3 || th_3 < MIN_ANGLE_3)return false;
 
-    cout << "pwh = " << pwh << endl;
-    cout << "pwz0 = " << pwz1 << endl;
-    cout << "pwz = " << pwz << endl;
-    cout << "c3 = " << c3 << endl;
-    cout << "s3 = " << s3 << endl;
-    cout << "th_3 = " << rad_th_3<< endl;
-    cout << "cp = " << cphi << endl;
-    cout << "p = " << phi << endl;
-    cout << "b = " << beta<< endl;
-    cout << "th_2 = " << rad_th_2 << endl;
-    cout << "th_1 = " << rad_th_1 << endl;
-    cout << "th_1 = " << rad_th_1 << endl;
-    cout << "a_th_1 = " << th_1 << endl;
-    cout << "a_th_2 = " << th_2 << endl;
-    cout << "a_th_3 = " << th_3 << endl;	
+    // cout << "pwh = " << pwh << endl;
+    // cout << "pwz0 = " << pwz1 << endl;
+    // cout << "pwz = " << pwz << endl;
+    // cout << "c3 = " << c3 << endl;
+    // cout << "s3 = " << s3 << endl;
+    // cout << "th_3 = " << rad_th_3<< endl;
+    // cout << "cp = " << cphi << endl;
+    // cout << "p = " << phi << endl;
+    // cout << "b = " << beta<< endl;
+    // cout << "th_2 = " << rad_th_2 << endl;
+    // cout << "th_1 = " << rad_th_1 << endl;
+    // cout << "th_1 = " << rad_th_1 << endl;
+    // cout << "a_th_1 = " << th_1 << endl;
+    // cout << "a_th_2 = " << th_2 << endl;
+    // cout << "a_th_3 = " << th_3 << endl;	
 	
 	return true;
 }
