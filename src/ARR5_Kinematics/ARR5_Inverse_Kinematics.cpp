a9d6761b0ebbad6b703522a10c472c416de55b5f
Paulo Victor <paulovictor237@gmail.com>
2019-06-20 23:43:03 -0300

    Adaptacao para Arduino
    -substituir cmath por math.h
    -remover iostream, stdio.h e "using namespace"
    - adicionar "Arduino.h" nos arquivos .cpp do espaço de trabalho e da cinematica inversa

--- src/ARR5_Kinematics/ARR5_Inverse_Kinematics.cpp
+++ src/ARR5_Kinematics/ARR5_Inverse_Kinematics.cpp
@@ -8,14 +8,12 @@
 #ifndef ARR5_INVERSE_KINEMATICS_CPP__
 #define ARR5_INVERSE_KINEMATICS_CPP__
 
+#include <Arduino.h>
 #include <stdio.h>
+#include <math>
 
-#include <cmath>
 #include "../ARR5_Data.h"
 #include "../ARR5_Angle_Conversions/degreesRadiansConversions.h"
-
-#include <iostream>
-using namespace std;
 
 // Funcao que calcula o quadrado.
 distance_t SQ(distance_t X){
@@ -40,13 +38,11 @@ robot_angles_t inverse_kinematics(distance_t d1, distance_t a1, distance_t a2, d
     distance_t pwz1=pwz;
     pwz = pwz - d1;
     
-
     float c3 = (SQ(pwh) + SQ(pwz) - SQ(a2) - SQ(a3)) / (2*a2*a3);
     //Verificar se o cosseno esta entre menos um e um!
     if(c3 > 1 || c3 < -1){
         // cout << "Erro: Fora do espaço de trabalho do robo." << endl;
     }
-
 
     float s3 = sqrt(abs(1-SQ(c3)))*elbow_up_or_down; // pode ser negativo
     float rad_th_3 = atan2(s3,c3);
@@ -74,57 +70,8 @@ robot_angles_t inverse_kinematics(distance_t d1, distance_t a1, distance_t a2, d
 	angles_out.theta_2 = th_2;
 	angles_out.theta_3 = th_3;
 
-    // cout << "pwh = " << pwh << endl;
-    // cout << "pwz0 = " << pwz1 << endl;
-    // cout << "pwz = " << pwz << endl;
-    // cout << "c3 = " << c3 << endl;
-    // cout << "s3 = " << s3 << endl;
-    // cout << "th_3 = " << rad_th_3<< endl;
-    // cout << "cp = " << cphi << endl;
-    // cout << "p = " << phi << endl;
-    // cout << "b = " << beta<< endl;
-    // cout << "th_2 = " << rad_th_2 << endl;
-    // cout << "th_1 = " << rad_th_1 << endl;
-    // cout << "th_1 = " << rad_th_1 << endl;
-    // cout << "a_th_1 = " << th_1 << endl;
-    // cout << "a_th_2 = " << th_2 << endl;
-    // cout << "a_th_3 = " << th_3 << endl;
-
 	return angles_out;
 }
 
-// int main(void)
-// {
-//     distance_t a1 = 14.53;
-//     distance_t a2 = 45.00;
-//     distance_t a3 = 83.00;
-//     distance_t d1 = 85;
-//     position_t point;
-//     // !! fora da área de espaco !!
-//     // point.x=143;
-//     // point.y=0;
-//     // point.z=85;
-
-//     // point.x =  43.044;
-//     // point.y = -118.26;
-//     // point.z =  149.00;
-    
-//     // point.x = -7.2405;
-//     // point.y = -82.759;
-//     // point.z =  134.52;
-
-//     // !! esses estão certos !!
-//     // point.x = 2.1656;
-//     // point.y = -12.282;
-//     // point.z = -9.3801;
-
-//     // point.x =  59.999;
-//     // point.y =  59.999;
-//     // point.z =  60.002;
-//     //basic_orientation_t ELBOW_UP ou ELBOW_DOWN
-//     robot_angles_t angles_out=inverse_kinematics(d1,a1,a2,a3,point,ELBOW_DOWN);
-
-// 	return 0;
-// }
 
 #endif /* end of include guard: ARR5_INVERSE_KINEMATICS_H_CPP */
--- src/ARR5_Kinematics/ARR5_Working_Space.cpp
+++ src/ARR5_Kinematics/ARR5_Working_Space.cpp
@@ -1,13 +1,13 @@
 #ifndef __ARR5_WORKING_SPACE_CPP__
 #define __ARR5_WORKING_SPACE_CPP__
 
+#include <Arduino.h>
+#include <stdio.h>
+#include <math>
 #include "../ARR5_Data.h"
 #include "../ARR5_Angle_Conversions/degreesRadiansConversions.h"
 #include "../ARR5_Angle_Conversions/ARR5_Joint_Limits.h"
-#include <stdio.h>
-#include <cmath>
-#include <iostream>
-using namespace std;
+
 // Funcao que calcula o quadrado.
 distance_t SQ(distance_t X);
 
@@ -36,7 +36,6 @@ bool is_point_on_working_space(distance_t d1, distance_t a1, distance_t a2, dist
     float s3 = sqrt(abs(1-SQ(c3)))*elbow_up_or_down; // pode ser negativo
     float rad_th_3 = atan2(s3,c3);
 
-
     float cphi = (SQ(pwh) + SQ(pwz) + SQ(a2) - SQ(a3)) / (2*a2*sqrt(abs(SQ(pwh)+SQ(pwz))));
     //Verificar se o cosseno esta entre menos um e um!
     if(cphi > 1 || cphi < -1)return false;
@@ -60,41 +59,8 @@ bool is_point_on_working_space(distance_t d1, distance_t a1, distance_t a2, dist
     if(th_1 > MAX_ANGLE_1 || th_1 < MIN_ANGLE_1)return false;
     if(th_2 > MAX_ANGLE_2 || th_2 < MIN_ANGLE_2)return false;
     if(th_3 > MAX_ANGLE_3 || th_3 < MIN_ANGLE_3)return false;
-
-    // cout << "pwh = " << pwh << endl;
-    // cout << "pwz0 = " << pwz1 << endl;
-    // cout << "pwz = " << pwz << endl;
-    // cout << "c3 = " << c3 << endl;
-    // cout << "s3 = " << s3 << endl;
-    // cout << "th_3 = " << rad_th_3<< endl;
-    // cout << "cp = " << cphi << endl;
-    // cout << "p = " << phi << endl;
-    // cout << "b = " << beta<< endl;
-    // cout << "th_2 = " << rad_th_2 << endl;
-    // cout << "th_1 = " << rad_th_1 << endl;
-    // cout << "th_1 = " << rad_th_1 << endl;
-    // cout << "a_th_1 = " << th_1 << endl;
-    // cout << "a_th_2 = " << th_2 << endl;
-    // cout << "a_th_3 = " << th_3 << endl;	
 	
 	return true;
 }
-// int main(void)
-// {
-//     distance_t a1 = 14.53;
-//     distance_t a2 = 45.00;
-//     distance_t a3 = 83.00;
-//     distance_t d1 = 85;
-//     position_t point;
-//     point.x=143;
-//     point.y=0;
-//     point.z=85;
-//     //basic_orientation_t ELBOW_UP ou ELBOW_DOWN
-//     if(is_point_on_working_space(d1,a1,a2,a3,point,ELBOW_DOWN))
-// 	cout << "true" << endl;
-// 	else cout << "false" << endl;
-
-// 	return 0;
-// }
 
 #endif
